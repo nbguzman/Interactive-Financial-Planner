97e0fa55ac993ee5279ebeb21fe44798ff6fc526
perroned <danny.perrone77@gmail.com>
2014-11-23 12:47:10 -0500

    Determine what the user is viewing

--- src/data/data.js
+++ src/data/data.js
@@ -6,3 +6,10 @@ data = [
 	{date:"26-Apr-12",close:"89.70"},
 	{date:"25-Apr-12",close:"99.00"}
 ];
+
+var EVENT_MODE = {
+	SPENDING: {VALUE: 0, name: "SPENDING"},
+	SAVING: {VALUE: 0, name: "SAVING"},
+	ROR: {VALUE: 0, name: "ROR"}
+};
+var newEventMode = EVENT_MODE.ROR;
